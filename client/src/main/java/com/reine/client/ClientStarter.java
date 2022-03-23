635936a214a95c7fed5db43da1c66100a26cabb7
Max Demydenko <demkom58@gmail.com>
2022-03-23 22:50:16 +0200

    feat: basic window creating

--- client/src/main/java/com/reine/client/ClientStarter.java
+++ client/src/main/java/com/reine/client/ClientStarter.java
@@ -1,7 +1,27 @@
 package com.reine.client;
 
+import com.reine.util.OperatingSystem;
+
+import static org.lwjgl.glfw.GLFW.*;
+
 public class ClientStarter {
     public static void main(String[] args) {
-        System.out.println("Client!");
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        if (OperatingSystem.current() == OperatingSystem.OSX) {
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+        }
+
+        long window = glfwCreateWindow(400, 300, "Reine", 0, 0);
+        if (window == 0) {
+            System.err.println("Failed to create GLFW window!");
+            glfwTerminate();
+            return;
+        }
+
+        glfwMakeContextCurrent(window);
     }
 }
