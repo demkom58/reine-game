4127beb54c475a91f647c2f3f75638762b738bb7
Max Demydenko <demkom58@gmail.com>
2022-03-23 22:49:21 +0200

    feat: enum for detecting OS

---
+++ common/src/main/java/com/reine/util/OperatingSystem.java
@@ -0,0 +1,35 @@
+package com.reine.util;
+
+import java.util.Locale;
+
+public enum OperatingSystem {
+    WINDOWS,
+    LINUX,
+    OSX,
+    SOLARIS,
+    UNKNOWN
+    ;
+
+    public static OperatingSystem current() {
+        String string = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        if (string.contains("win")) {
+            return OperatingSystem.WINDOWS;
+        }
+        if (string.contains("mac")) {
+            return OperatingSystem.OSX;
+        }
+        if (string.contains("solaris")) {
+            return OperatingSystem.SOLARIS;
+        }
+        if (string.contains("sunos")) {
+            return OperatingSystem.SOLARIS;
+        }
+        if (string.contains("linux")) {
+            return OperatingSystem.LINUX;
+        }
+        if (string.contains("unix")) {
+            return OperatingSystem.LINUX;
+        }
+        return OperatingSystem.UNKNOWN;
+    }
+}
